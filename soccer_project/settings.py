730e738b804e49b6bf55c72eaa63cfffc2eb7c7d
mohamedrizo-hub <mohamedrizo2009@gmail.com>
2026-04-19 14:00:50 +0300

    first commit

--- soccer_project/settings.py
+++ soccer_project/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "soccer_project",
 
 ]
 
@@ -49,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'soccer_project',
+
 ]
 
 ROOT_URLCONF = 'soccer_project.urls'
